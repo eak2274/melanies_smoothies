c3bde8e51e6fe252bceda4687c18d6afa9073b47
eak2274 <117574920+eak2274@users.noreply.github.com>
2025-09-30 12:24:57 +0000

    Restart app

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,11 +2,11 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 
-st.warning("⏸️ Приложение временно остановлено.")
-st.stop()
+# st.warning("⏸️ Приложение временно остановлено.")
+# st.stop()
 
 # Write directly to the app
-st.title("My Parents New Healthy Dinner")
+st.title(f":cup_with_straw: Customize your smoothie :cup_with_straw: {st.__version__}")
 st.write(
   """Choose the fruit you want in your custom Smoothie!
   """
