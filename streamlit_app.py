59619bbf0c303eb5bfc5b3fff2377fbadb7d4d15
eak2274 <117574920+eak2274@users.noreply.github.com>
2025-10-03 11:26:48 +0000

    Add pandas, debug mode=on

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
+import pandas as pd 
 
 # st.warning("⏸️ Приложение временно остановлено.")
 # st.stop()
@@ -28,8 +29,15 @@ except Exception as e:
     st.stop()
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit_name'),col('search_on'))
-st.dataframe(data=my_dataframe, use_container_width=True)
+# st.dataframe(data=my_dataframe, use_container_width=True)
+# st.stop()
+
+# Convert the Snowpark Dataframe to Pandas Dataframe so we can use the LOC function
+pd_df = my_dataframe.to_pandas()
+st.dataframe(pd_df)
 st.stop()
+
+
 ingredients_list=st.multiselect(
     "Choose up to five ingredients:",
     my_dataframe,
